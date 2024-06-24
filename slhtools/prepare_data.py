60292f773b3a250a722944acbbfc45a11fb7dd9b
Anubhab Haldar <6704979+Chronum94@users.noreply.github.com>
2024-06-24 09:37:18 -0700

    lmao

---
+++ slhtools/prepare_data.py
@@ -0,0 +1,197 @@
+import json
+from pathlib import Path
+from typing import Union
+
+import ase
+import numpy as np
+from ase.data import atomic_numbers
+from ase.io import read
+from ase.units import Ha
+
+from slhtools.utils import (
+    BlockedMatrix,
+    get_neighbourlist_ijD,
+    make_hamiltonian_blockedmatrix,
+)
+
+
+# TODO this function does a bit more than it needs to.
+def prepare_gpaw_slh_snapshot(
+    directory,
+    *,
+    gpwfilename: str,
+    cutoff: float,
+    overlap_only=False,
+    overlap_calc_parameters=None,
+    reference_dtype=np.float32,
+    reference_arraytype="sparse",
+    block_write_threshold=1e-4,
+):
+    atoms = read(gpwfilename)
+    directory = Path(directory)
+    directory.mkdir(exist_ok=True, parents=True)
+
+    write_only_atoms(directory, atoms)
+
+    # This gets us ij (atom1 atom2) matrix elements as we desire
+    # with a cutoff that we decide. If there are nonzero hamiltonian matrix
+    # elements outside this cutoff, for the moment, we don't care.
+    ij, D = get_neighbourlist_ijD(atoms, cutoff)
+
+    if not overlap_only:
+        (
+            H_MM,
+            S_MM,
+            ls_dict,
+        ) = get_hamiltonian_and_basis_information_from_gpw(gpwfilename)
+
+    elif overlap_only:
+        assert overlap_calc_parameters is not None
+        H_MM = None
+        fermi_levels = None
+        (S_MM, ls_dict, basis_permutation_dict) = get_overlap_and_basis_information(
+            atoms, overlap_calc_parameters
+        )
+
+    # If overlap_only, H_MM should be none.
+    # If not overlap_only, H_MM should NOT be none.
+    # We do this with an XNOR operator such that True XNOR True = False XNOR False  = True
+    assert not (overlap_only ^ (H_MM is None))
+    H_MM = H_MM.astype(reference_dtype) if H_MM is not None else None
+    S_MM = S_MM.astype(reference_dtype)
+
+    if reference_arraytype == "sparse":
+        from scipy.sparse import csr_matrix
+
+        np.savez(
+            directory / "H_MM_S_MM",
+            **{
+                "H_MM": csr_matrix(H_MM) if H_MM is not None else None,
+                "S_MM": csr_matrix(S_MM),
+            },
+        )
+    elif reference_arraytype == "dense":
+        np.savez(directory / "H_MM_S_MM", **{"H_MM": H_MM, "S_MM": S_MM})
+    else:
+        raise ValueError(
+            "Unknown reference arraytype, must be either 'sparse' or 'dense'."
+        )
+
+    block_sizes = np.array([sum(2 * np.array(ls_dict[n]) + 1) for n in atoms.numbers])
+    assert np.sum(block_sizes) == len(S_MM), f"{np.sum(block_sizes)}, {len(S_MM)}"
+
+    
+    blocked_hamiltonian = make_hamiltonian_blockedmatrix(
+            S_MM if overlap_only else H_MM,
+            atoms,
+            block_sizes,
+        )
+    ij, D, hlist = filter_pairs_by_hblock_magnitude(
+        ij,
+        D,
+        blocked_hamiltonian=blocked_hamiltonian,
+        threshold=block_write_threshold,
+    )
+    np.savez(directory / "ijD.npz", ij=ij, D=D)
+    np.savez(directory / "hblocks.npz", hblocks=np.array(hlist, dtype=object), allow_pickle=True)
+
+    with open(directory / "orbital_ells.json", mode="w") as fd:
+        json.dump(ls_dict, fd)
+
+
+def write_only_atoms(directory, atoms):
+    from ase.io import write
+
+    with open(directory / "atoms.extxyz", mode="w") as fd:
+        write(fd, atoms)
+
+
+def get_basis_indices_from_calculation(calculation):
+    """Given a GPAW DFTCalculation object, calculates a dict of ell and ordered lm indices
+    for each elemental block in an LCAO hamiltonian.
+
+    Parameters
+    ----------
+    calculation : DFTCalculation
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    setups = calculation.setups
+    ls_dict = {}
+
+    # The setups contain basis information about the calculation
+    for setup in setups:
+        ells = [x.l for x in setup.basis.bf_j]
+        ls_dict[atomic_numbers[setup.basis.symbol]] = ells
+
+    return ls_dict
+
+
+def get_overlap_and_basis_information(atoms, parameters):
+    from gpaw.new.calculation import DFTCalculation
+
+    calculation = DFTCalculation.from_parameters(atoms, parameters)
+    ls_dict, basisdict = get_basis_indices_from_calculation(calculation)
+    # TODO: Would be nice to be able to get the k-space S_MM and then FT back
+    S_MM = calculation.state.ibzwfs.wfs_qs[0][0].S_MM
+    S_MM.gather()
+
+    return S_MM.data, ls_dict, basisdict
+
+
+def get_hamiltonian_and_basis_information_from_gpw(gpwfilename: str):
+    from gpaw.new.ase_interface import GPAW
+
+    # We set scalapack here even one a single core since that enables
+    # sparse atomic corrections.
+    calculation = GPAW(gpwfilename, parallel={"sl_auto": True}).calculation
+    ls_dict = get_basis_indices_from_calculation(calculation)
+    matcalc = calculation.scf_loop.hamiltonian.create_hamiltonian_matrix_calculator(
+        calculation.state
+    )
+
+    # TODO This is spin-paired and gamma point for the moment.
+    H_MM = matcalc.calculate_matrix(calculation.state.ibzwfs.wfs_qs[0][0])
+    H_MM.gather()
+    S_MM = calculation.state.ibzwfs.wfs_qs[0][0].S_MM
+    S_MM.gather()
+
+    return (
+        H_MM.data * Ha,
+        S_MM.data,
+        ls_dict,
+        # calculation.state.ibzwfs.fermi_levels * Ha,
+    )
+
+
+def filter_pairs_by_hblock_magnitude(
+    ij, D, blocked_hamiltonian,threshold
+):
+    
+    filtered_ij_list = []
+    filtered_D_list = []
+    matrix_blocks_list = []
+    for _ij, _D in zip(ij, D, strict=True):
+        i, j = _ij
+
+        matrix_block = blocked_hamiltonian.get_block(i, j)
+        # A max abs is the infinity-norm
+        if np.max(np.abs(matrix_block)) >= threshold:
+            filtered_ij_list.append(_ij)
+            filtered_D_list.append(_D)
+            matrix_blocks_list.append(matrix_block)
+
+    return np.array(filtered_ij_list), np.array(filtered_D_list), matrix_blocks_list
+    # TODO Need to generate diagonal blocks
+    # for ii in np.unique(ij[:, 0]):
+    #     # if ii == 1:
+    #     #     np.savetxt("dump.txt", blocked_hamiltonian.get_block(ii, ii).ravel())
+    #     # Self-overlap terms will always have a lattice shift of 0, 0, 0
+    #     tmp = [0, 0, 0, int(ii + 1), int(ii + 1)]
+    #     h5handle.create_dataset(
+    #         json.dumps(tmp), data=blocked_hamiltonian.get_block(ii, ii)
+    #     )
---
+++ slhtools/utils.py
@@ -0,0 +1,180 @@
+from typing import List, Union
+
+import ase
+import numpy as np
+from matscipy.neighbours import neighbour_list
+
+
+def get_neighbourlist_ijD(
+    atoms: ase.Atoms, cutoff: float, unique_pairs: bool = True
+):
+    """Returns a array where each row is atom-pair indices and an array of lattice shifts for that pair
+
+    Parameters
+    ----------
+    atoms : ase.Atoms
+        _description_
+    cutoff : float
+        _description_
+    unique_pairs : bool, optional
+        _description_, by default True
+    """
+    i, j, D = neighbour_list("ijD", atoms=atoms, cutoff=cutoff)
+    ij = np.column_stack([i, j]).astype(int)
+
+    if unique_pairs:
+        for ii in range(len(atoms)):
+            all_neighbours = ij[ij[:, 0] == ii, 1]
+            unique_neighbours = set(all_neighbours)
+            assert len(unique_neighbours) == len(
+                all_neighbours
+            ), f"Atom {ii} has non-unique neighbours with respect to lattice shifts!"
+
+    return ij, D
+
+
+class BlockedHamiltonian:
+    def __init__(
+        self,
+        atoms: ase.Atoms,
+        nbasis_species_dict: dict,
+        hamiltonian,
+        orbitals_permutation_dict=None,
+    ):
+        """_summary_
+
+        Parameters
+        ----------
+        atoms : ase.Atoms
+            _description_
+        nbasis_species_dict : dict
+            Number of basis functions for each element in the hamiltonian
+        hamiltonian : np.ndarray
+            2D array, the hamiltonian matrix
+        orbitals_permutation_dict : dict
+            Orbital permutation vector for each element's basis functions.
+        """
+        self.atoms = atoms
+        self.nbasis_species_dict = nbasis_species_dict
+        self.basis_starts = (
+            np.cumsum([nbasis_species_dict[s] for s in atoms.symbols])
+            - nbasis_species_dict[atoms[0].symbol]
+        )
+        self.hamiltonian = hamiltonian
+        self.opd = orbitals_permutation_dict
+        self.permute = False if self.opd is None else True
+
+    def get_block(self, i, j):
+        atoms_i_symbol = self.atoms[i].symbol
+        atoms_j_symbol = self.atoms[j].symbol
+        istart = self.basis_starts[i]
+        istop = istart + self.nbasis_species_dict[atoms_i_symbol]
+        jstart = self.basis_starts[j]
+        jstop = jstart + self.nbasis_species_dict[atoms_j_symbol]
+        try:
+            hblock = (
+                self.permute_rowcols(
+                    self.hamiltonian[istart:istop, jstart:jstop],
+                    self.opd[atoms_i_symbol],
+                    self.opd[atoms_j_symbol],
+                )
+                if self.permute
+                else self.hamiltonian[istart:istop, jstart:jstop]
+            )
+            return hblock
+        except KeyError:
+            print(list(self.opd.keys()))
+
+
+class BlockedMatrix:
+    def __init__(
+        self,
+        matrix,
+        block_ids,
+        block_sizes,
+        permutation_dict=None,
+    ):
+        """Represents a minimal BlockedMatrix structure so we can call
+        the (i, j)-th block of a block structured matrix. Pretty much any
+        matrix made out of atom-wise basis functions will look like this.
+
+        Parameters
+        ----------
+        matrix : np.ndarray
+            The underlying 2D matrix.
+        block_ids : int array
+            Unique block identifiers for each block. Corresponds to 'atoms' for LCAO Hamiltonians, but is more general.
+        block_sizes : int array
+            Array of block sizes. This in principle can be a block_id : block_size dict, but it isn't.
+        permutation_dict : dict, optional
+            A block_id : int array dictionary to reorder rows/columns of each block, by default None
+        """
+        self.matrix = matrix
+        self.block_ids = block_ids
+        self.block_sizes = block_sizes
+        self.block_starts = np.concatenate([[0], np.cumsum(block_sizes[:-1])])
+        self.pd = permutation_dict
+
+    def get_block(self, i, j):
+        istart = self.block_starts[i]
+        istop = istart + self.block_sizes[i]
+        jstart = self.block_starts[j]
+        jstop = jstart + self.block_sizes[j]
+        try:
+            block = self.matrix[istart:istop, jstart:jstop]
+            return block
+        except Exception as e:
+            raise e
+
+    def permute_rowcols(self, block, prows, pcols):
+        # This particular part of the code doesn't need to be performant
+        # but it does need to tell us if there's something going wrong.
+        assert len(np.unique(prows)) == len(prows)
+        assert len(np.unique(pcols)) == len(pcols)
+        assert (
+            len(prows) == block.shape[0]
+        ), f"{len(prows)} elements in permutation but only {block.shape[0]} rows in Hamiltonian block!"
+        assert (
+            len(pcols) == block.shape[1]
+        ), f"{len(pcols)} elements in permutation but only {block.shape[1]} cols in Hamiltonian block!"
+
+        return block[prows, :][:, pcols]
+
+
+def make_hamiltonian_blockedmatrix(
+    H_MM, atoms, basis_block_sizes
+):
+    H_MM_blocked = BlockedMatrix(
+        H_MM, atoms.numbers, basis_block_sizes
+    )
+    return H_MM_blocked
+
+
+class VectorPermuter:
+    def __init__(self, from_array, to_array):
+        assert len(from_array) == len(
+            to_array
+        ), "Can only permute between equal-length iterables"
+        assert set(from_array) == set(
+            to_array
+        ), "All elements of each iterable must be unique"
+        permutation_matrix = to_array[:, None] == from_array[None, :]
+        self._f = np.argwhere(permutation_matrix)[:, 1]
+        self._b = np.argwhere(permutation_matrix.T)[:, 1]
+
+    def forward(self, from_array):
+        return from_array[self._f]
+
+    def backward(self, to_array):
+        return to_array[self._b]
+
+
+# GPAW to OMX permutation dict for m's
+# This is taken from DeepH-E3's data processing module
+# not much else to do here
+gpw2omx_lp = {
+    0: np.array([0]),
+    1: np.nonzero(np.eye(3, dtype=int)[[1, 2, 0]].T)[1],
+    2: np.nonzero(np.eye(5, dtype=int)[[2, 4, 0, 3, 1]].T)[1],
+    3: np.nonzero(np.eye(7, dtype=int)[[6, 4, 2, 0, 1, 3, 5]].T)[1],
+}
